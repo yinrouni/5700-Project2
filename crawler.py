b069531c3fd555f88a3c88ad72a0e1be9c0b6246
rouni.yin <yin.ro@husky.neu.edu>
2020-09-29 21:50:06 -0400

    feat: login

--- crawler.py
+++ crawler.py
@@ -14,14 +14,14 @@ def generaterHeader(method, path, cookie, data):
         prefix = "%s %s HTTP/1.1\r\nHost: %s\r\n" % (method, path, HOST)
 
     if cookie and data:
-        return ("%sCookie: %s\r\n\r\n%s" % (prefix, cookie, data)).encode();
+        return ("%sCookie: %s\r\n\r\n%s" % (prefix, cookie, data)).encode()
     elif not data and cookie:
-        return ("%sCookie: %s\r\n\r\n" % (prefix, cookie)).encode();
+        return ("%sCookie: %s\r\n\r\n" % (prefix, cookie)).encode()
 
     elif data and not cookie:
-        return ("%s\r\n%s" % (prefix, data)).encode();
+        return ("%s\r\n%s" % (prefix, data)).encode()
 
-    return ("%s\r\n" % prefix).encode();
+    return ("%s\r\n" % prefix).encode()
 
 
 def getCookie():
@@ -40,7 +40,6 @@ def getCookie():
             if not recv:
                 break
             response = recv.decode().split('\r\n\r\n')
-            print(recv.decode())
 
         csrfToken = re.findall('csrftoken=[^;]*;', response[0])[0]
         sessionId = re.findall('sessionid=[^;]*;', response[0])[0]
@@ -62,40 +61,42 @@ def login(cookie):
         client_socket.connect(server_address)
 
         # TODO: replace credentials
-        request_header = generaterHeader("POST", '/accounts/login/', cookie, 'username=001445026&password=JHXZX0UI&next=%2Ffakebook%2F&csrfmiddlewaretoken='+ cookie.split(";")[0].split('=')[1])
+        request_header = generaterHeader("POST", '/accounts/login/', cookie,
+                                         'username=001445026&password=JHXZX0UI&next=%2Ffakebook%2F&csrfmiddlewaretoken=' +
+                                         cookie.split(";")[0].split('=')[1])
         client_socket.sendall(request_header)
 
         response = []
         while True:
             recv = client_socket.recv(2048)
             if not recv:
-                break
+                continue
             response = recv.decode().split('\r\n\r\n')
-            # print(recv.decode())
+            break
 
-        # csrfToken = re.findall('csrftoken=[^;]*;', response[0])[0]
         sessionId = re.findall('sessionid=[^;]*;', response[0])[0]
 
         # print(csrfToken)
-        print(sessionId)
+        # print(sessionId)
 
         tmp = cookie.split(";")
         tmp[1] = sessionId[:-1]
 
-        login_header = generaterHeader("GET", "/fakebook/", ';'.join(tmp), None)
-        # login_header = ("GET /fakebook/ HTTP/1.1\r\nHost: cs5700fa20.ccs.neu.edu\r\n"
-        #                 "DNT: 1\r\nReferer: http://cs5700.ccs.neu.edu/accounts/login/?next=/fakebook/\r\nCookie: "+';'.join(tmp)+"\r\nConnection: keep-alive\r\n\r\n").encode()
-        print(login_header.decode())
-        client_socket.sendall(login_header)
+        # renew cookie
+        cookie = ';'.join(tmp)
+        login_header = generaterHeader("GET", "/fakebook/", cookie, None)
 
+        client_socket.sendall(login_header)
         while True:
             recv = client_socket.recv(2048)
-            print(recv.decode())
+
             if not recv:
-                print('no res')
-                break
+                continue
             response = recv.decode().split('\r\n\r\n')
 
+            break
+        print(response[0])
+        print(response[1])
 
 
     except Exception:
